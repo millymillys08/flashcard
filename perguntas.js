d94ed17116d49c78fe291c9985da6f986b211fbb
millymillys08 <00001085359232sp@al.educacao.sp.gov.br>
2024-11-25 12:01:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual foi a primeira civilização conhecida da história?',
+    'Mesopotâmicos'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a causa principal da primeira Guerra Mundial?',
+    'Nacionalismo, Imperialismo e Alianças Militares'
+)
+
+criaCartao(
+    'História',
+    'Qual civilização contruio as pirâmides do Egito?',
+    'Egípicios'
+)
+
+criaCartao(
+    'Hiatória',
+    'Em que ano começou a Segunda Guerra Mundial?',
+    '1939'
+)
